afcc230a9b4c5749e7cc8cfd88bbb8ed398bb370
HyperCreeck@Cadex <52355164+asiawin@users.noreply.github.com>
2020-07-21 20:18:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
-from colorama import init 
-from termcolor import colored 
+# Python program to print 
+# colored text and background 
+import sys 
+from termcolor import colored, cprint 
   
-init() 
+text = colored('Hello, World!', 'red', attrs=['reverse', 'blink']) 
+print(text) 
+cprint('Hello, World!', 'green', 'on_red') 
   
-print(colored('Hello, World!', 'green', 'on_red')) 
+print_red_on_cyan = lambda x: cprint(x, 'red', 'on_cyan') 
+print_red_on_cyan('Hello, World!') 
+print_red_on_cyan('Hello, Universe!') 
+  
+for i in range(10): 
+    cprint(i, 'magenta', end=' ') 
+  
+cprint("Attention!", 'red', attrs=['bold'], file=sys.stderr) 
